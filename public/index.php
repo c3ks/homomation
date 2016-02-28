8a4b0ab117de4ccc418ed61643d95cc4eb7f0bac
Philipp Hirsch <itself@hanspolo.net>
2016-02-28 09:29:27 +0100

    Switch to ruby on rails

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$f3 = require('../lib/base.php');
-$f3->set('AUTOLOAD','../src/');
-$f3->set('UI','../views/');
-
-$f3->route('GET /', 'Main\Home->show');
-
-$f3->run();
--- src/main/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Main;
-
-class Home {
-  function show($f3) {
-    echo \Template::instance()->render('main/home/show.html');
-  }
-}
